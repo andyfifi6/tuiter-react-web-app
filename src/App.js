60f807843140bb0b637cb7c0e5acf806d52659f3
andyfifi6 <andyfifi6@gmail.com>
2022-09-14 16:32:29 -0700

    modified  src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web Development Fall 2022 Yingrui Yang
         </p>
         <a
           className="App-link"
