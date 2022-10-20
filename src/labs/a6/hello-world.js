250924c35bff99a44152de80d5a1b947ca1323a1
andyfifi6 <andyfifi6@gmail.com>
2022-10-19 23:42:23 -0700

    a6 lab

--- src/labs/a6/hello-world.js
+++ src/labs/a6/hello-world.js
@@ -1,14 +1,9 @@
-import {Link}
-    from "react-router-dom";
 import Nav from "../../nav";
 
 function HelloWorld() {
     return(
         <div>
             <Nav/>
-            {/*<Link to="/">Lab</Link> |*/}
-            {/*<Link to="/hello">Hello</Link> |*/}
-            {/*<Link to="/tuiter">Tuiter</Link>*/}
             <h1>Hello World!</h1>
         </div>
     );
