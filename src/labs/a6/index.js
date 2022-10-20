3b7ba37b7ffb56ccfbb5fe5ae6310b385b24fb9a
andyfifi6 <andyfifi6@gmail.com>
2022-10-19 23:33:54 -0700

    a6 lab done

---
+++ src/labs/a6/index.js
@@ -0,0 +1,26 @@
+import {Link}
+    from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+    from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
+
+
+function Assignment6() {
+    return(
+        <div>
+            {/*<Link to="/">Lab</Link> |*/}
+            {/*<Link to="/hello">Hello</Link> |*/}
+            {/*<Link to="/tuiter">Tuiter</Link>*/}
+            <h1>Assignment 6</h1>
+            <TodoList/>
+            <TodoItem/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </div>
+    )
+}
+export default Assignment6;
